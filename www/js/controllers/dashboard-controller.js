36a0aa832b3bb4ac72700570e5705e6e28f62ddd
Zoe Ames <zoeames@gmail.com>
2015-01-12 10:56:19 -0600

    profile dashboard completed

--- www/js/controllers/dashboard-controller.js
+++ www/js/controllers/dashboard-controller.js
@@ -2,6 +2,12 @@
   'use strict';
   angular.module('starter')
   .controller('DashCtrl', function($rootScope, $scope, $state, User){
+    if($rootScope.rootuser.isadmin === 'true'){
+      $scope.institutionStatus='Professor';
+    }else{
+      $scope.institutionStatus='Student';
+    }
+
     $scope.logout = function(){
       User.logout().then(function(){
         $rootScope.rootuser = null;
